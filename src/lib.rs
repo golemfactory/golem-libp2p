5a2840e7c879aa785db25694c41af7f3fec12abb
badb <a.skrzy@gmail.com>
2019-02-05 23:17:04 +0100

    connect libp2p with ethkey

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,11 @@
-extern crate gu_ethkey;
+extern crate ethkey;
 extern crate libp2p;
-extern crate tokio_codec;
-extern crate tokio_io;
 
 use std::env;
 use std::io::Error as IoError;
 use libp2p::SimpleProtocol;
 use libp2p::secio::{SecioConfig, SecioKeyPair};
-use tokio_codec::{BytesCodec, Framed};
-use tokio_io::{AsyncRead, AsyncWrite};
 
-pub const DEFAULT_ADDRESS: &str = "/ip4/127.0.0.1/tcp/10333";
-pub const PROTOCOL: &str = "/echo/1.0.0";
-pub const DUMMY_MSG: &str = "SYN";
-pub const DUMMY_MSG2: &str = "ACK";
-
-pub fn get_address_from_arg() -> String {
-	env::args().nth(1).unwrap_or(DEFAULT_ADDRESS.to_owned())
-}
 
 pub fn generate_keypair() -> SecioKeyPair {
 	SecioKeyPair::secp256k1_generated().expect("Wrongly generated keys")
@@ -27,20 +15,3 @@ pub fn get_secio_upgrade() -> SecioConfig {
 	SecioConfig::new(generate_keypair())
 }
 
-pub fn get_protocol<TSocket>(name: String) -> 
-	SimpleProtocol<impl Fn(TSocket) -> 
-		Result<Framed<TSocket, BytesCodec>, IoError>> 
-	where TSocket: AsyncRead + AsyncWrite
-{
-	SimpleProtocol::new(name, |socket| {
-		Ok(Framed::new(socket, BytesCodec::new()))
-	})
-}
-
-pub fn get_golem_protocol<TSocket>() -> 
-	SimpleProtocol<impl Fn(TSocket) -> 
-		Result<Framed<TSocket, BytesCodec>, IoError>>
-	where TSocket: AsyncRead + AsyncWrite
-{
-	get_protocol(PROTOCOL.to_string())
-}
