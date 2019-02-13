12c4c4c1fa32f3dba0d1a20dea0c59b64de3f022
Adam Wierzbicki <awierzbicki@golem.network>
2019-02-13 14:19:03 +0100

    Re-connect behaviour

--- src/behaviour/mod.rs
+++ src/behaviour/mod.rs
@@ -1,1 +1,2 @@
+pub mod reconnect;
 pub mod welcome;
--- src/bin/welcome_demo.rs
+++ src/bin/welcome_demo.rs
@@ -2,12 +2,49 @@ use std::env;
 
 use futures::stream::Stream;
 
-use libp2p::core::swarm::Swarm;
+use libp2p::NetworkBehaviour;
+use libp2p::core::swarm::{NetworkBehaviourEventProcess, Swarm};
 use libp2p::secio::SecioKeyPair;
 
 use tokio;
+use tokio_io::{AsyncRead, AsyncWrite};
 
+use void::Void;
+
+use golem_libp2p::behaviour::reconnect::ReconnectBehaviour;
 use golem_libp2p::behaviour::welcome::WelcomeBehaviour;
+
+// Network behaviour composing welcome and re-connect behaviours
+#[derive(NetworkBehaviour)]
+struct MyBehaviour<TSubstream> {
+    welcome: WelcomeBehaviour<TSubstream>,
+    reconnect: ReconnectBehaviour<TSubstream>
+}
+
+impl<TSubstream> MyBehaviour<TSubstream> {
+    fn new (welcome_message: String) -> Self {
+        MyBehaviour {
+            welcome: WelcomeBehaviour::new(welcome_message),
+            reconnect: ReconnectBehaviour::new()
+        }
+    }
+}
+
+impl<TSubstream> NetworkBehaviourEventProcess<String> for MyBehaviour<TSubstream>
+where
+    TSubstream: AsyncRead + AsyncWrite
+{
+    fn inject_event(&mut self, event: String) {
+        println!("Welcome message received: {}", event)
+    }
+}
+
+impl<TSubstream> NetworkBehaviourEventProcess<Void> for MyBehaviour<TSubstream>
+where
+    TSubstream: AsyncRead + AsyncWrite
+{
+    fn inject_event(&mut self, _: Void) {}
+}
 
 fn main() {
     let local_private_key = SecioKeyPair::ed25519_generated().unwrap();
@@ -15,7 +52,7 @@ fn main() {
     println!("My ID: {:?}", local_peer_id);
 
     let transport = libp2p::build_development_transport(local_private_key);
-    let behaviour = WelcomeBehaviour::new("Hello!".to_owned());
+    let behaviour = MyBehaviour::new("Hello!".to_owned());
     let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
 
     let args: Vec<String> = env::args().collect();
@@ -34,9 +71,6 @@ fn main() {
 
     tokio::run(swarm
         .map_err(|e| eprintln!("{}", e))
-        .for_each(|message| {
-            println!("Received message: {}", message);
-            Ok(())
-        })
+        .for_each(|_| Ok(()))
     );
 }
